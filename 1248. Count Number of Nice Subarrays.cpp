ea0d13041eed38696fd4655d6abfe8fa38494ccb
nigam-adarsh2003 <119562732+nigam-adarsh2003@users.noreply.github.com>
2024-06-22 23:55:30 +0530

    Create 1248. Count Number of Nice Subarrays.cpp

---
+++ 1248. Count Number of Nice Subarrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int oddCount = 0;
+        int count    = 0;
+        int result   = 0;
+
+        int i = 0;
+        int j = 0;
+        while(j < n) {
+            if(nums[j] % 2 != 0) { 
+                oddCount++;
+                count = 0; 
+            }
+            while(oddCount == k) {
+                count++;
+                if(i < n && nums[i] % 2 == 1) { 
+                    oddCount--;
+                }
+                i++;
+            }
+            result += count;
+            j++;
+        }
+        return result;
+    }
+};
